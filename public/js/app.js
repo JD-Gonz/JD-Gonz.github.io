80565666eb521c268879d89f2c94079219e339ec
JD Gonzalez <jdg99@live.com>
2017-05-14 19:29:57 +0000

    updated contact using angular + added resume

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ app.controller('AboutCtrl', ['$scope', function($scope) {
       'title' : 'Background',
       'content' : 'I grew up in a small town in central Washington, and have been living in Seattle for about seven years now. I came to Seattle in pursuit of higher education at the University of Washington, where I earned a degree in Informatics with a focus in Information Assurance and Cybersecurity. During my time at the University of Washington, I also worked for PEMCO Insurance on a three-month summer internship and Informion as a Software Developer part-time.'
     }, {
-      'title' : 'Developer In Training?',
+      'title' : 'Developer In Training',
       'content' : 'I thought that the tag-line “Developer in Training” was a rather quaint one to use while studying in college. It also sounded like it would be an appropriate phrase while I was still being mentored. I have quickly realized that software developers never really stop learning. We always have to “train” for the next big thing – or, better yet, invent it.'
     }
   ];
@@ -67,7 +67,42 @@ app.controller('AboutCtrl', ['$scope', function($scope) {
 }]);
 
 app.controller('ContactCtrl', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
+  $scope.resume = {
+    'title' : 'REVIEW MY RESUME!',
+    'content' : 'DOWNLOAD RESUME',
+    'class' : 'fa fa-cloud-download'
+  };
+  
+  $scope.email = {
+    'title' : 'EMAIL',
+    'content' : 'DGONZ001@uw.edu'
+  };
+  
+  $scope.phone = {
+    'title' : 'PHONE',
+    'content' : '509.264.0988'
+  };
+  
+  $scope.web = {
+    'title' : 'ON THE WEB',
+    'content' : [{
+      'name' : 'freeCodeCamp',
+      'class' : 'glyphicon glyphicon-tent',
+      'href' : 'https://www.freecodecamp.com/dgonz001'
+    }, {
+      'name' : 'Github',
+      'class' : 'fa fa-github fa-fw',
+      'href' : 'https://github.com/dgonz001'
+    }, {
+      'name' : 'Linkedin',
+      'class' : 'fa fa-linkedin fa-fw',
+      'href' : 'https://www.linkedin.com/in/jdannygonzalez'
+    }, {
+      'name' : 'Twitter',
+      'class' : 'fa fa-twitter fa-fw',
+      'href' : 'https://twitter.com/danny_b0y03'
+    }]
+  };
 }]);
 
 app.controller('PortfolioCtrl', ['$scope', function($scope) {
