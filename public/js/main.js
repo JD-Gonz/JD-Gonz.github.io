8a877ab0063909aa5626f2109a5926d391240e75
JD Gonzalez <dgonz001@uw.edu>
2023-04-16 22:42:07 -0700

    update css and JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
-(function ($) {
+angular.module('PersonalWebsite', []);
 
+(function ($) {
   var $window = $(window),
     $body = $('body'),
     settings = {
@@ -10,7 +11,6 @@
         fadeIn: true,
         fadeDelay: 250
       },
-
     };
 
   // Breakpoints.
@@ -71,13 +71,11 @@
 
   // Carousels.
   $('.carousel').each(function () {
-
     var $t = $(this),
       $forward = $('<span class="forward"></span>'),
       $backward = $('<span class="backward"></span>'),
       $reel = $t.children('.reel'),
       $items = $reel.children('article');
-
     var pos = 0,
       leftLimit,
       rightLimit,
@@ -87,36 +85,25 @@
 
     // Items.
     if (settings.carousels.fadeIn) {
-
       $items.addClass('loading');
-
       $t.scrollex({
         mode: 'middle',
         top: '-20vh',
         bottom: '-20vh',
         enter: function () {
-
           var timerId,
             limit = $items.length - Math.ceil($window.width() / itemWidth);
-
           timerId = window.setInterval(function () {
             var x = $items.filter('.loading'), xf = x.first();
-
             if (x.length <= limit) {
-
               window.clearInterval(timerId);
               $items.removeClass('loading');
               return;
-
             }
-
             xf.removeClass('loading');
-
           }, settings.carousels.fadeDelay);
-
         }
       });
-
     }
 
     // Main.
@@ -126,7 +113,6 @@
       leftLimit = 0;
       $t._updatePos();
     };
-
     $t._updatePos = function () { $reel.css('transform', 'translate(' + pos + 'px, 0)'); };
 
     // Forward.
@@ -141,7 +127,6 @@
             window.clearInterval(timerId);
             pos = rightLimit;
           }
-
           $t._updatePos();
         }, 10);
       })
@@ -156,14 +141,10 @@
       .mouseenter(function (e) {
         timerId = window.setInterval(function () {
           pos += settings.carousels.speed;
-
           if (pos >= leftLimit) {
-
             window.clearInterval(timerId);
             pos = leftLimit;
-
           }
-
           $t._updatePos();
         }, 10);
       })
@@ -173,40 +154,27 @@
 
     // Init.
     $window.on('load', function () {
-
       reelWidth = $reel[0].scrollWidth;
-
       if (browser.mobile) {
-
         $reel
           .css('overflow-y', 'hidden')
           .css('overflow-x', 'scroll')
           .scrollLeft(0);
         $forward.hide();
         $backward.hide();
-
       }
       else {
-
         $reel
           .css('overflow', 'visible')
           .scrollLeft(0);
         $forward.show();
         $backward.show();
-
       }
-
       $t._update();
-
       $window.on('resize', function () {
         reelWidth = $reel[0].scrollWidth;
         $t._update();
       }).trigger('resize');
-
     });
-
   });
-
-
-
 })(jQuery);
